352a56231e0d4fc2fe09ca577c8ff1001473551a
coolnavjot31 <coolnavjot31@gmail.com>
2023-02-19 21:33:16 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TuplesPractice;
+
+Console.WriteLine("Hello, World!");
+SimpleTuple simpleTuple = new SimpleTuple();
+var result = simpleTuple.Process();
+Console.WriteLine(result.Item1.FirstOrDefault());
+foreach(var item in result.Item1)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine(result.Item1.FirstOrDefault());
+Console.ReadLine();
+
---
+++ SimpleTuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuplesPractice
+{
+    public class SimpleTuple
+    {
+        public Tuple<string[], int[], int> Process() 
+        {
+            string[] strArray = { "ABC", "XYZ" };
+            int[] intArray = { 1,4,2,5,3,6};
+            Tuple<string[], int[], int> tuple = new Tuple<string[], int[], int>(strArray,intArray,1);
+            return tuple;
+        }
+
+        public(int, int)  Process1()
+        {
+            return (1, 1);
+        }
+    }
+}
